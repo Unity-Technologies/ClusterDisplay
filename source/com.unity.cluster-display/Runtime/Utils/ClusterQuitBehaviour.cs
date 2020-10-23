bce7a238aba0510affc4d9be0377ef57aebda63e
Brad Weiers <bradley@unity3d.com>
2020-10-23 13:39:25 -0700

    Add documentation to cluster quit

--- source/com.unity.cluster-display/Runtime/Utils/ClusterQuitBehaviour.cs
+++ source/com.unity.cluster-display/Runtime/Utils/ClusterQuitBehaviour.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace ClusterDisplay.Utils
 {
+	/// <summary>
+	/// Listen to input command to quit the application on all nodes
+	/// </summary>
+	/// <remarks>If this is not present present upon receiving the quit message the nodes will only disconnect, but not quit the applciation.</remarks>
     public class ClusterQuitBehaviour : MonoBehaviour
     {
         private void Update()
