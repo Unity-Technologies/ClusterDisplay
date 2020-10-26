61c603521ce56b2d6ce8b7b4e5b8f477451f2e59
Yohann Vaast <yohann.vaast@unity3d.com>
2020-10-26 14:21:20 -0400

    Add comments for QuadroSync documentation.

--- source/com.unity.cluster-display/Runtime/QuadroSync/GfxPluginQuadroSyncSystem.cs
+++ source/com.unity.cluster-display/Runtime/QuadroSync/GfxPluginQuadroSyncSystem.cs
@@ -7,17 +7,58 @@ using UnityEngine.Rendering;
 [assembly: InternalsVisibleTo("Unity.ClusterDisplay.Editor.Tests")]
 namespace Unity.ClusterDisplay
 {
+	/// <summary>
+    /// This class is responsible for controlling the QuadroSync technology.
+    /// </summary>
     public class GfxPluginQuadroSyncSystem
     {
+		/// <summary>
+        /// Define the various QuadroSync states and the use of the SwapGroup and Swapbarrier system.
+        /// </summary>
         public enum EQuadroSyncRenderEvent
         {
+			/// <summary>
+			/// Enable the Workstation SwapGroup and the use of
+			/// the Swap Group and the Swap Barrier systems (NvAPI).
+			/// </summary>
             QuadroSyncInitialize = 0,
+			
+			/// <summary>
+			/// Query the actual frame count in Runtime for the Master Sync system
+			/// or for the custom frame count system.
+			/// </summary>
             QuadroSyncQueryFrameCount,
+			
+			/// <summary>
+			/// Reset the frame count for the Master Sync system (NvAPI) or
+			/// for the custom frame count system.
+			/// </summary>
             QuadroSyncResetFrameCount,
+			
+			/// <summary>
+			/// Disable the use of the Swap Group and the Swap Barrier systems
+			/// and disable the Workstation SwapGroup (NvAPI)
+			/// </summary>
             QuadroSyncDispose,
+			
+			/// <summary>
+			/// Enable or disable the use of the Swap Group and the Swap Barrier systems (NvAPI).
+			/// </summary>
             QuadroSyncEnableSystem,
+			
+			/// <summary>
+			/// Enable or disable the use of the Swap Group system (NvAPI).
+			/// </summary>
             QuadroSyncEnableSwapGroup,
+			
+			/// <summary>
+			/// Enable or disable the use of the Swap Barrier system (NvAPI).
+			/// </summary>
             QuadroSyncEnableSwapBarrier,
+			
+			/// <summary>
+			/// Enable or disable the use of the Master sync counter system (NvAPI).
+			/// </summary>
             QuadroSyncEnableSyncCounter
         };
 
@@ -34,6 +75,9 @@ namespace Unity.ClusterDisplay
             public static extern IntPtr GetRenderEventFuncD3D12();
         }
 
+		/// <summary>
+        /// Unique instance of the class (Singleton). 
+        /// </summary>
         public static GfxPluginQuadroSyncSystem Instance
         {
             get { return instance; }
@@ -48,6 +92,11 @@ namespace Unity.ClusterDisplay
         {
         }
 
+		/// <summary>
+        /// Execute a CommandBuffer related to the EQuadroSyncRenderEvent.
+        /// </summary>
+		 /// <param name="id"> QuadroSync command to execute.</param>
+		 /// <param name="data"> Data bound to the executed command.</param>
         public void ExecuteQuadroSyncCommand(EQuadroSyncRenderEvent id, IntPtr data)
         {
             var cmdBuffer = new CommandBuffer();
