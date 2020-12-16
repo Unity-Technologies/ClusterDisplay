13ed58d68f97186df2a102990658e9cf480fff8e
Yohann Vaast <59579629+YohannVaastUnity@users.noreply.github.com>
2020-12-16 10:16:49 -0500

    Fix QuadroSync integration errors (#29)
    * Upgrade project, upgrade packages, change Test project (cube faster, more lags).
    
    * Fix DLLNotFoundException error.
    
    * Test if the path passes Yamato & Change project name
    
    * Add new C++ plugin changes + fix indentation (CS and C++) + fix DLL path error.
    
    Co-authored-by: Brad Weiers <bradley@unity3d.com>
    Co-authored-by: Vlad Lazar <vladal@unity3d.com>

--- GfxPluginQuadroSyncD3D12/D3D12QuadroSync.h
+++ GfxPluginQuadroSyncD3D12/D3D12QuadroSync.h
@@ -10,43 +10,43 @@ class IDXGISwapChain;
 
 namespace GfxQuadroSync {
 
-    class PluginCSwapGroupClient
-    {
-    public:
-        PluginCSwapGroupClient();
-        ~PluginCSwapGroupClient();
+	class PluginCSwapGroupClient
+	{
+	public:
+		PluginCSwapGroupClient();
+		~PluginCSwapGroupClient();
 
-        void Prepare();
-        bool Initialize(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain);
-        void Dispose(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain);
+		void Prepare();
+		bool Initialize(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain);
+		void Dispose(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain);
 
-        void SetupWorkStation();/// const;
-        void DisposeWorkStation();/// const;
+		void SetupWorkStation();/// const;
+		void DisposeWorkStation();/// const;
 
-        bool Render(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain, int pVsync = 1, int pFlags = 0);// const;
-        void ResetFrameCount(ID3D12Device* pDevice);
-        NvU32 QueryFrameCount(ID3D12Device* pDevice);
+		bool Render(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain, int pVsync = 1, int pFlags = 0);// const;
+		void ResetFrameCount(ID3D12Device* pDevice);
+		NvU32 QueryFrameCount(ID3D12Device* pDevice);
 
-        void EnableSystem(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain, bool value);
-        void EnableSwapGroup(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain, bool value);
-        void EnableSwapBarrier(ID3D12Device* pDevice, bool value);
-        void EnableSyncCounter(const bool value);
+		void EnableSystem(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain, bool value);
+		void EnableSwapGroup(ID3D12Device* pDevice, IDXGISwapChain* pSwapChain, bool value);
+		void EnableSwapBarrier(ID3D12Device* pDevice, bool value);
+		void EnableSyncCounter(const bool value);
 
-    private:
-        NvU32 m_GroupId;
-        NvU32 m_BarrierId;
-        NvU32 m_FrameCount;
-        NvU32 m_GSyncSwapGroups;
-        NvU32 m_GSyncBarriers;
-        bool  m_GSyncMaster;
-        bool  m_GSyncCounter;
-        bool  m_IsActive;
-        bool  m_Initialized;
+	private:
+		NvU32 m_GroupId;
+		NvU32 m_BarrierId;
+		NvU32 m_FrameCount;
+		NvU32 m_GSyncSwapGroups;
+		NvU32 m_GSyncBarriers;
+		bool  m_GSyncMaster;
+		bool  m_GSyncCounter;
+		bool  m_IsActive;
+		bool  m_Initialized;
 
 #ifdef DEBUG_LOG_QUADRO_SYNC
-        HANDLE m_fileDebug;
-        void WriteFileDebug(const char* message, bool append = true);
+		HANDLE m_fileDebug;
+		void WriteFileDebug(const char* message, bool append = true);
 #endif
-    };
-    
+	};
+
 }
