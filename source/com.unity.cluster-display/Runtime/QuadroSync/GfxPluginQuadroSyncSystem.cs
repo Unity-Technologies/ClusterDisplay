99bee35fe7076d07a0ce4d517c881d979e5b8c6d
Yohann Vaast <59579629+YohannVaastUnity@users.noreply.github.com>
2021-01-07 10:50:06 -0500

    Package validation fixes (#28)
    * Add documentation to cluster quit
    
    * Fix license file for package validation
    
    * Add comments for QuadroSync documentation.
    
    * Fix indent and change commentaries
    
    * Fix DLLNotFoundEXception
    
    * Upgrade project, upgrade packages, change Test project (cube faster, more lags).
    
    * Revert "Upgrade project, upgrade packages, change Test project (cube faster, more lags)."
    
    This reverts commit bf8c4bb50ea696c66f68ff6f9a88200a161ef270.
    
    * Remove non-pertinent changes.
    
    Co-authored-by: Brad Weiers <bradley@unity3d.com>
    Co-authored-by: Vlad Lazar <vladal@unity3d.com>

--- source/com.unity.cluster-display/Runtime/QuadroSync/GfxPluginQuadroSyncSystem.cs
+++ source/com.unity.cluster-display/Runtime/QuadroSync/GfxPluginQuadroSyncSystem.cs
@@ -7,17 +7,58 @@ using UnityEngine.Rendering;
 [assembly: InternalsVisibleTo("Unity.ClusterDisplay.Editor.Tests")]
 namespace Unity.ClusterDisplay
 {
+    /// <summary>
+    /// This class is responsible for controlling the QuadroSync technology.
+    /// </summary>
     public class GfxPluginQuadroSyncSystem
     {
+        /// <summary>
+        ///  Parameters to define the various QuadroSync states and the use of the SwapGroup and SwapBarrier system.
+        /// </summary>
         public enum EQuadroSyncRenderEvent
         {
+            /// <summary>
+            /// Enables the Workstation SwapGroup and the use of
+            /// the Swap Group and the Swap Barrier systems (NvAPI).
+            /// </summary>
             QuadroSyncInitialize = 0,
+
+            /// <summary>
+            /// Queries the actual frame count in Runtime for the Master Sync system
+            /// or for the custom frame count system.
+            /// </summary>
             QuadroSyncQueryFrameCount,
+
+            /// <summary>
+            /// Resets the frame count for the Master Sync system (NvAPI) or
+            /// for the custom frame count system.
+            /// </summary>
             QuadroSyncResetFrameCount,
+
+            /// <summary>
+            /// Disables the use of the Swap Group and the Swap Barrier systems
+            /// and disables the Workstation SwapGroup (NvAPI)
+            /// </summary>
             QuadroSyncDispose,
+
+            /// <summary>
+            /// Enables or disables the use of the Swap Group and the Swap Barrier systems (NvAPI).
+            /// </summary>
             QuadroSyncEnableSystem,
+
+            /// <summary>
+            /// Enables or disables the use of the Swap Group system (NvAPI).
+            /// </summary>
             QuadroSyncEnableSwapGroup,
+
+            /// <summary>
+            /// Enables or disables the use of the Swap Barrier system (NvAPI).
+            /// </summary>
             QuadroSyncEnableSwapBarrier,
+
+            /// <summary>
+            /// Enables or disables the use of the Master sync counter system (NvAPI).
+            /// </summary>
             QuadroSyncEnableSyncCounter
         };
 
@@ -42,6 +83,9 @@ namespace Unity.ClusterDisplay
             public static extern IntPtr GetRenderEventFuncD3D12();
         }
 
+        /// <summary>
+        /// Gets the unique instance of the class (Singleton). 
+        /// </summary>
         public static GfxPluginQuadroSyncSystem Instance
         {
             get { return instance; }
@@ -56,6 +100,11 @@ namespace Unity.ClusterDisplay
         {
         }
 
+        /// <summary>
+        /// Executes a CommandBuffer related to the EQuadroSyncRenderEvent.
+        /// </summary>
+        /// <param name="id"> QuadroSync command to execute.</param>
+        /// <param name="data"> Data bound to the executed command.</param>
         public void ExecuteQuadroSyncCommand(EQuadroSyncRenderEvent id, IntPtr data)
         {
             var cmdBuffer = new CommandBuffer();
