1e2eb3b8489e762364916bddbc478f5e456b80a2
Sean Connor <sean.connor@unity3d.com>
2021-03-29 11:23:50 -0600

    Commenting out XR.

--- source/com.unity.cluster-display.graphics/Runtime/Layout/ILayoutBuilder.cs
+++ source/com.unity.cluster-display.graphics/Runtime/Layout/ILayoutBuilder.cs
@@ -6,10 +6,12 @@ using UnityEngine.Rendering.HighDefinition;
 namespace Unity.ClusterDisplay.Graphics
 {
     // Interface to be implemented by ClusterRenderer's custom layouts. 
+    /*
     interface ILayoutBuilder
     {
         bool BuildLayout(XRLayout layout, ClusterRenderContext context, ScriptableCullingParameters cullingParams);
         void Initialize();
         void Dispose();
     }
+    */
 }
