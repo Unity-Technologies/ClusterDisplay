e6fee71e44a39dc85572218b0a19ec1d5827c56b
Sean Connor <sean.connor@unity3d.com>
2021-04-15 09:59:11 -0600

    Cleaning up old code, attempting to force update editor on load.

--- source/com.unity.cluster-display.graphics/Runtime/CameraConfigurator/HDRPClusterBufferController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Rendering;
-
-namespace Unity.ClusterDisplay.Graphics
-{
-    public abstract class HDRPPresenter : Presenter {}
-}
