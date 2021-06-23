1f15d2f1600d6b3c8a3c74a9d21fbe0e1c055667
Sean Connor <sean.connor@unity3d.com>
2021-06-23 10:38:18 -0600

    Removing commented out old code.

--- source/com.unity.cluster-display.graphics/Runtime/Layout/RTManager/TileRTManager.cs
+++ source/com.unity.cluster-display.graphics/Runtime/Layout/RTManager/TileRTManager.cs
@@ -16,55 +16,5 @@ namespace Unity.ClusterDisplay.Graphics
         public abstract T GetPresentRT(int width, int height, GraphicsFormat format = GraphicsFormat.B8G8R8A8_SRGB);
         public abstract T GetBackBufferRT(int width, int height, GraphicsFormat format = GraphicsFormat.B8G8R8A8_SRGB);
         public abstract void Release();
-
-        /*
-        public RenderTexture GetSourceRenderTexture (int width, int height, GraphicsFormat format = GraphicsFormat.R8G8B8A8_SRGB)
-        {
-            var rt = GetBlitRT(width, height, format);
-            if (!(rt is RenderTexture))
-                throw new System.InvalidOperationException("RT is not a RenderTexture.");
-            return rt as RenderTexture;
-        }
-
-        public RTHandle GetBlitRTHandle (int width, int height, GraphicsFormat format = GraphicsFormat.R8G8B8A8_SRGB)
-        {
-            var rt = GetBlitRT(width, height, format);
-            if (!(rt is RTHandle))
-                throw new System.InvalidOperationException("RT is not a RTHandle.");
-            return rt as RTHandle;
-        }
-
-        public RenderTexture GetPresentRenderTexture (int width, int height, GraphicsFormat format = GraphicsFormat.R8G8B8A8_SRGB)
-        {
-            var rt = GetPresentRT(width, height, format);
-            if (!(rt is RenderTexture))
-                throw new System.InvalidOperationException("RT is not a RenderTexture.");
-            return rt as RenderTexture;
-        }
-
-        public RTHandle GetPresentRTHandle (int width, int height, GraphicsFormat format = GraphicsFormat.R8G8B8A8_SRGB)
-        {
-            var rt = GetPresentRT(width, height, format);
-            if (!(rt is RTHandle))
-                throw new System.InvalidOperationException("RT is not a RTHandle.");
-            return rt as RTHandle;
-        }
-
-        public RenderTexture GetBackBufferRenderTexture (int width, int height, GraphicsFormat format = GraphicsFormat.R8G8B8A8_SRGB)
-        {
-            var rt = GetBackBufferRT(width, height, format);
-            if (!(rt is RenderTexture))
-                throw new System.InvalidOperationException("RT is not a RenderTexture.");
-            return rt as RenderTexture;
-        }
-
-        public RTHandle GetBackBufferRTHandle (int width, int height, GraphicsFormat format = GraphicsFormat.R8G8B8A8_SRGB)
-        {
-            var rt = GetBackBufferRT(width, height, format);
-            if (!(rt is RTHandle))
-                throw new System.InvalidOperationException("RT is not a RTHandle.");
-            return rt as RTHandle;
-        }
-        */
     }
 }
