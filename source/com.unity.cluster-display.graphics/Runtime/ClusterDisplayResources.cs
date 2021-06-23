613d892b35743d8afedd4310ab99d4a264fea901
Sean Connor <sean.connor@unity3d.com>
2021-06-23 10:02:09 -0600

    Suppressing unassigned field warning that is set in the editor.

--- source/com.unity.cluster-display.graphics/Runtime/ClusterDisplayResources.cs
+++ source/com.unity.cluster-display.graphics/Runtime/ClusterDisplayResources.cs
@@ -7,7 +7,9 @@ namespace Unity.ClusterDisplay.Graphics
     [CreateAssetMenu(fileName = "Data", menuName = "Cluster Display/ClusterDisplayResources", order = 1)]
     public class ClusterDisplayResources : ScriptableObject
     {
+        #pragma warning disable 0649
         [SerializeField] private Material m_BlitMaterial;
+        #pragma warning restore 0649
         public Material blitMaterial => m_BlitMaterial;
     }
 }
