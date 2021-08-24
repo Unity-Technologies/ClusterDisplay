0dba9a38158291d8726b5f1a64a7cc6ee16f8296
Sean Connor <sean.connor@unity3d.com>
2021-08-24 16:09:57 -0700

    Reorganizing samples, added skinned mesh VFX sample.

---
+++ source/com.unity.cluster-display.samples/Runtime/Samples/TransformHierarchyStreaming/Scripts/DisableAnimatorIfRepeaterNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.ClusterDisplay;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DisableAnimatorIfRepeaterNode : MonoBehaviour
+{
+    private Animator animator;
+    private void OnValidate() =>
+        animator = GetComponent<Animator>();
+
+    private void OnEnable() =>
+        animator.enabled = !ClusterDisplayState.IsClusterLogicEnabled || ClusterDisplayState.IsRepeater;
+}
