bb362ec5cf1fbf15307663f1edcc4351edaf5f6f
Sean Igor Connor <sean.connor@unity3d.com>
2021-12-13 18:58:17 -0700

    Making sure that camera overrding is disabled on destroy/disable in test.

--- source/com.unity.cluster-display.samples/Runtime/Samples/OverrideCameraProjection/Scripts/OverrideCameraProjection.cs
+++ source/com.unity.cluster-display.samples/Runtime/Samples/OverrideCameraProjection/Scripts/OverrideCameraProjection.cs
@@ -15,12 +15,36 @@ namespace Unity.ClusterDisplay
             clusterRenderer.preRenderCameraDataOverride -= PreRenderCameraDataOverride;
             clusterRenderer.preRenderCameraDataOverride += PreRenderCameraDataOverride;
         }
-
+        
         private void PreRenderCameraDataOverride(int nodeId, ref Vector3 position, ref Quaternion rotation, ref Matrix4x4 matrix)
         {
             position.x += nodeId - 0.5f;
             rotation = Quaternion.AngleAxis((nodeId - 0.5f) * 45f, Vector3.up);
             matrix = Matrix4x4.Perspective(45f, 16 / 9f, 0.1f, 100f);
+        }
+
+        private void OnEnable()
+        {
+            if (!ClusterRenderer.TryGetInstance(out var clusterRenderer))
+                return;
+            
+            clusterRenderer.preRenderCameraDataOverride -= PreRenderCameraDataOverride;
+            clusterRenderer.preRenderCameraDataOverride += PreRenderCameraDataOverride;
+        }
+
+        private void OnDisable()
+        {
+            if (!ClusterRenderer.TryGetInstance(out var clusterRenderer))
+                return;
+            
+            clusterRenderer.preRenderCameraDataOverride -= PreRenderCameraDataOverride;
+        }
+        
+        private void OnDestroy()
+        {
+            if (!ClusterRenderer.TryGetInstance(out var clusterRenderer, throwError: false))
+                return;
+            clusterRenderer.preRenderCameraDataOverride -= PreRenderCameraDataOverride;
         }
     }
 }
