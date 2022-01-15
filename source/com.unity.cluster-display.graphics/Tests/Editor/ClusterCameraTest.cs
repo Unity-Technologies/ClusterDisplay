4e26cb44a63c6eb00c688374a8771496644e3942
etienne cella <etienne.p.cella@gmail.com>
2022-01-14 20:35:40 -0500

    gameview size, cleanup

--- source/com.unity.cluster-display.graphics/Tests/Editor/ClusterCameraTest.cs
+++ source/com.unity.cluster-display.graphics/Tests/Editor/ClusterCameraTest.cs
@@ -1,46 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
-using Unity.ClusterDisplay.Graphics;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.TestTools;
 
-public class ClusterCameraTest
+namespace Unity.ClusterDisplay.Graphics.EditorTests
 {
-    List<Camera> m_Cameras = new();
+    public class ClusterCameraTest
+    {
+        List<Camera> m_Cameras = new();
 
-    [OneTimeSetUp]
-    public void LoadEmptyScene()
-    {
-        EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
-    }
-    
-    [SetUp]
-    public void SetUp()
-    {
-        m_Cameras.Add(new GameObject("camera1", typeof(ClusterCamera)).GetComponent<Camera>());
-        m_Cameras.Add(new GameObject("camera2", typeof(ClusterCamera)).GetComponent<Camera>());
-    }
-
-    [TearDown]
-    public void TearDown()
-    {
-        foreach (var camera in m_Cameras)
+        [OneTimeSetUp]
+        public void LoadEmptyScene()
         {
-            Object.DestroyImmediate(camera.gameObject);
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
         }
-    }
-    
-    [Test]
-    public void TracksActiveCamera()
-    {
-        m_Cameras[0].gameObject.SetActive(false);
-        Assert.That(ClusterCameraManager.Instance.ActiveCamera, Is.EqualTo(m_Cameras[1]));
-        m_Cameras[1].gameObject.SetActive(false);
-        Assert.That(ClusterCameraManager.Instance.ActiveCamera, Is.Null);
-        m_Cameras[0].gameObject.SetActive(true);
-        Assert.That(ClusterCameraManager.Instance.ActiveCamera, Is.EqualTo(m_Cameras[0]));
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_Cameras.Add(new GameObject("camera1", typeof(ClusterCamera)).GetComponent<Camera>());
+            m_Cameras.Add(new GameObject("camera2", typeof(ClusterCamera)).GetComponent<Camera>());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            foreach (var camera in m_Cameras)
+            {
+                Object.DestroyImmediate(camera.gameObject);
+            }
+        }
+
+        [Test]
+        public void TracksActiveCamera()
+        {
+            m_Cameras[0].gameObject.SetActive(false);
+            Assert.That(ClusterCameraManager.Instance.ActiveCamera, Is.EqualTo(m_Cameras[1]));
+            m_Cameras[1].gameObject.SetActive(false);
+            Assert.That(ClusterCameraManager.Instance.ActiveCamera, Is.Null);
+            m_Cameras[0].gameObject.SetActive(true);
+            Assert.That(ClusterCameraManager.Instance.ActiveCamera, Is.EqualTo(m_Cameras[0]));
+        }
     }
 }
