2f157ab3d245651ff7c4b923eef06145d3928d9f
Sean Igor Connor <sean.connor@unity3d.com>
2022-02-01 12:44:24 -0500

    Removing secondary packages.

--- source/com.unity.cluster-display.samples/Runtime/Samples/Common/Scripts/DontDestroyGameObjectOnUnload.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class DontDestroyGameObjectOnUnload : MonoBehaviour
-{
-    private void Awake() =>
-        DontDestroyOnLoad(gameObject);
-}
--- source/com.unity.cluster-display.samples/Runtime/Samples/FPS/Scripts/Game/IgnoreHeatMap.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace Unity.FPS.Game
-{
-    public class IgnoreHeatMap : MonoBehaviour
-    {
-    }
-}
