f078d41aa45b52a51980ec31cacfc1c205f96e3f
Sean Igor Connor <sean.connor@unity3d.com>
2022-04-01 14:48:34 -0400

    Removing all logic regarding GUI, wrappers, source generators to the com.unity.cluster-display.helpers package to simplify com.unity.cluster-display.rpc

--- source/com.unity.cluster-display.rpc/Runtime/AssemblyInfo.cs
+++ source/com.unity.cluster-display.rpc/Runtime/AssemblyInfo.cs
@@ -1,7 +1,12 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Unity.ClusterDisplay.GeneratedWrappers")]
 [assembly: InternalsVisibleTo("Unity.ClusterDisplay.RPC.Editor")]
+
+[assembly: InternalsVisibleTo("Unity.ClusterDisplay.GeneratedWrappers")]
+
 [assembly: InternalsVisibleTo("Unity.ClusterDisplay.Editor.CodeGen")]
 [assembly: InternalsVisibleTo("Unity.ClusterDisplay.Editor.Inspectors.Generated")]
+
+[assembly: InternalsVisibleTo("Unity.ClusterDisplay.Helpers")]
+[assembly: InternalsVisibleTo("Unity.ClusterDisplay.Helpers.Editor")]
 
