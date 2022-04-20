c6bfbcc845d22113f9e07c1201cb8d252cd8c437
Wen Zhang <wen.zhang@unity3d.com>
2022-04-20 13:47:17 -0400

    Rebase with dev.

--- source/com.unity.cluster-display/Runtime/States/HardwareSyncInitState.cs
+++ source/com.unity.cluster-display/Runtime/States/HardwareSyncInitState.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 
 namespace Unity.ClusterDisplay
 {
+    /// <summary>
+    /// Hardware synchronization state.
+    /// </summary>
+    /// <remarks>
+    /// This state should be executed before the handshake.
+    /// <see cref="HardwareSyncInitState"/> is a "null" state (does nothing).
+    /// Derive from this class and return an instance of the child in
+    /// <see cref="Create"/> to perform actual initialization of hardware.
+    /// </remarks>
     class HardwareSyncInitState : NodeState
     {
         public static NodeState Create(IClusterSyncState clusterSyncState)
         {
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
-            return new QuadroSyncInitState(clusterSyncState);
+            return CommandLineParser.disableQuadroSync.Value
+                ? new HardwareSyncInitState(clusterSyncState)
+                : new QuadroSyncInitState(clusterSyncState);
 #else
             return new HardwareSyncInitState(clusterSyncState);
 #endif
