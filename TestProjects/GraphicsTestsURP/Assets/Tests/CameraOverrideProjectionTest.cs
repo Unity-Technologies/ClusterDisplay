077016d961079b9c352bcc86856f1e00055b4376
Sean Igor Connor <sean.connor@unity3d.com>
2022-05-09 16:59:02 -0400

    Removing namespace.

--- TestProjects/GraphicsTestsURP/Assets/Tests/CameraOverrideProjectionTest.cs
+++ TestProjects/GraphicsTestsURP/Assets/Tests/CameraOverrideProjectionTest.cs
@@ -6,60 +6,36 @@ using Unity.ClusterDisplay.Graphics.Tests;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using Unity.ClusterDisplay.Graphics;
 
-namespace Unity.ClusterDisplay.Graphics.Tests
+public class CameraOverrideProjectionTest : ClusterRendererTestReferenceCamera
 {
-    public class CameraOverrideProjectionTest : ClusterRendererTestReferenceCamera
+    [OneTimeSetUp]
+    public void LoadScene()
     {
-        [OneTimeSetUp]
-        public void LoadScene()
+        SceneManager.LoadScene("CameraOverrideProjection");
+    }
+
+    static IEnumerable<string> VolumeProfileNames => Utils.VolumeProfileNames;
+
+    // Note that LensDistortion is not in this collection.
+    // Overscan does its job of removing artefacts at the edge,
+    // but the vanilla capture will retain the artefact making the test fail.
+    static IEnumerable<string> VolumeProfileOverscanSupportNames
+    {
+        get
         {
-            SceneManager.LoadScene("CameraOverrideProjection");
+            yield return "Bloom";
+            yield return "ChromaticAberration";
+            yield return "Vignette";
         }
+    }
 
-        static IEnumerable<string> VolumeProfileNames => Utils.VolumeProfileNames;
-
-        // Note that LensDistortion is not in this collection.
-        // Overscan does its job of removing artefacts at the edge,
-        // but the vanilla capture will retain the artefact making the test fail.
-        static IEnumerable<string> VolumeProfileOverscanSupportNames
-        {
-            get
+    [UnityTest]
+    public IEnumerator CompareReferenceAndCameraOverride([ValueSource("VolumeProfileNames")] string profileName)
+    {
+        yield return CompareReferenceAndCluster(profileName, () =>
             {
-                yield return "Bloom";
-                yield return "ChromaticAberration";
-                yield return "Vignette";
-            }
-        }
-
-        [UnityTest]
-        public IEnumerator CompareReferenceAndCameraOverride([ValueSource("VolumeProfileNames")] string profileName)
-        {
-            yield return CompareReferenceAndCluster(profileName, () =>
-                {
-                    // Set up the projection with the override properties
-                    var cameraTransform = m_ReferenceCamera.transform;
-                    var projection = m_ClusterRenderer.ProjectionPolicy as CameraOverrideProjection;
-                    Assert.That(projection, Is.Not.Null);
-                    projection.Overrides = CameraOverrideProjection.OverrideProperty.All;
-                    projection.Position = cameraTransform.position;
-                    projection.Rotation = cameraTransform.rotation;
-                    projection.NodeID = 0;
-                    projection.ProjectionMatrix = m_ReferenceCamera.projectionMatrix;
-                },
-                profileName == "FilmGrain"
-                    ? () => Debug.LogError(
-                        "Film grain test requires the LWRP_DEBUG_STATIC_POSTFX scripting symbol to be defined in the Player Settings.")
-                    : null);
-        }
-
-        [UnityTest]
-        public IEnumerator CompareReferenceAndCameraOverrideWithOverscan([ValueSource("VolumeProfileOverscanSupportNames")] string profileName)
-        {
-            yield return CompareReferenceAndCluster(profileName, () =>
-            {
-                m_ClusterRenderer.Settings.OverScanInPixels = 64;
-
                 // Set up the projection with the override properties
                 var cameraTransform = m_ReferenceCamera.transform;
                 var projection = m_ClusterRenderer.ProjectionPolicy as CameraOverrideProjection;
@@ -69,7 +45,29 @@ namespace Unity.ClusterDisplay.Graphics.Tests
                 projection.Rotation = cameraTransform.rotation;
                 projection.NodeID = 0;
                 projection.ProjectionMatrix = m_ReferenceCamera.projectionMatrix;
-            });
-        }
+            },
+            profileName == "FilmGrain"
+                ? () => Debug.LogError(
+                    "Film grain test requires the LWRP_DEBUG_STATIC_POSTFX scripting symbol to be defined in the Player Settings.")
+                : null);
+    }
+
+    [UnityTest]
+    public IEnumerator CompareReferenceAndCameraOverrideWithOverscan([ValueSource("VolumeProfileOverscanSupportNames")] string profileName)
+    {
+        yield return CompareReferenceAndCluster(profileName, () =>
+        {
+            m_ClusterRenderer.Settings.OverScanInPixels = 64;
+
+            // Set up the projection with the override properties
+            var cameraTransform = m_ReferenceCamera.transform;
+            var projection = m_ClusterRenderer.ProjectionPolicy as CameraOverrideProjection;
+            Assert.That(projection, Is.Not.Null);
+            projection.Overrides = CameraOverrideProjection.OverrideProperty.All;
+            projection.Position = cameraTransform.position;
+            projection.Rotation = cameraTransform.rotation;
+            projection.NodeID = 0;
+            projection.ProjectionMatrix = m_ReferenceCamera.projectionMatrix;
+        });
     }
 }
