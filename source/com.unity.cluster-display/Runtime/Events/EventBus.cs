0d1a112b96b89d12226d390e741dd35e87a4cabc
Wen Zhang <wen.zhang@unity3d.com>
2022-05-18 09:10:06 -0400

    Deterministic id-gen. Doc fixup.

--- source/com.unity.cluster-display/Runtime/Events/EventBus.cs
+++ source/com.unity.cluster-display/Runtime/Events/EventBus.cs
@@ -4,33 +4,34 @@ using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using Unity.Collections;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 namespace Unity.ClusterDisplay
 {
     /// <summary>
-    /// This class provides a strongly-typed API for propagating data across the cluster
+    /// This class provides a strongly-typed API for propagating events across the cluster
     /// (from Emitter to Repeaters).
     /// </summary>
     /// <remarks>
     /// EventBus wraps <see cref="EmitterStateWriter.RegisterOnStoreCustomDataDelegate"/> and
     /// <see cref="RepeaterStateReader.RegisterOnLoadDataDelegate"/>.
     /// </remarks>
-    /// <typeparam name="T"></typeparam>
-    class EventBus<T> : IDisposable where T : unmanaged
+    /// <typeparam name="TEvent">Type of the events managed by this bus.</typeparam>
+    class EventBus<TEvent> : IDisposable where TEvent : unmanaged
     {
         /// <summary>
-        /// Delegate for receiving a contiguous array of objects.
+        /// Delegate for receiving a contiguous array of event objects.
         /// </summary>
-        public delegate void BulkEventListener(ReadOnlySpan<T> eventData);
+        public delegate void BulkEventListener(ReadOnlySpan<TEvent> eventData);
 
         const int k_MaxBuffer = 128;
         int m_OutBufferLength;
 
-        NativeArray<T> m_OutBuffer = new(k_MaxBuffer, Allocator.Persistent);
-        readonly List<Action<T>> m_Listeners = new();
+        NativeArray<TEvent> m_OutBuffer = new(k_MaxBuffer, Allocator.Persistent);
+        readonly List<Action<TEvent>> m_Listeners = new();
         readonly List<BulkEventListener> m_BulkListeners = new();
 
-        static readonly int k_FrameDataId = (typeof(T).GetHashCode() * 397) ^ (int) StateID.CustomData;
+        static readonly int k_FrameDataId = (int)StateID.CustomData ^ typeof(TEvent).DeterministicTypeID();
 
         internal ReadOnlySpan<byte> OutBuffer =>
             MemoryMarshal.AsBytes(m_OutBuffer.GetSubArray(0, m_OutBufferLength).AsReadOnlySpan());
@@ -59,11 +60,11 @@ namespace Unity.ClusterDisplay
         }
 
         /// <summary>
-        /// Enqueues data on the bus. The data is propagated during the next sync point.
+        /// Enqueues an event on the bus. The event is propagated during the next sync point.
         /// </summary>
-        /// <param name="eventData"></param>
-        /// <returns></returns>
-        public bool Publish(T eventData)
+        /// <param name="eventData">The event data to be published.</param>
+        /// <returns><see langword="false"/> if the queue is full. <see langword="true"/> if successfully queued.</returns>
+        public bool Publish(TEvent eventData)
         {
             if (m_OutBufferLength < m_OutBuffer.Length - 1)
             {
@@ -77,19 +78,19 @@ namespace Unity.ClusterDisplay
         /// <summary>
         /// Listen for events.
         /// </summary>
-        /// <param name="listener"></param>
+        /// <param name="listener">The event listener.</param>
         /// <returns>An object that can be used to unsubscribe from the bus when disposed.</returns>
-        public IDisposable Subscribe(Action<T> listener)
+        public IDisposable Subscribe(Action<TEvent> listener)
         {
             m_Listeners.Add(listener);
-            return new Unsubscriber<Action<T>>(m_Listeners, listener);
+            return new Unsubscriber<Action<TEvent>>(m_Listeners, listener);
         }
 
         /// <summary>
-        /// Listen for an "array" of events. This is efficient if the listener processes the data in bulk.
+        /// Listen for an "array" of events. This is efficient if the listener processes the event data in bulk.
         /// </summary>
         /// <param name="bulkListener"></param>
-        /// <returns></returns>
+        /// <returns>An object that can be used to unsubscribe from the bus when disposed.</returns>
         public IDisposable Subscribe(BulkEventListener bulkListener)
         {
             m_BulkListeners.Add(bulkListener);
@@ -103,43 +104,51 @@ namespace Unity.ClusterDisplay
         /// You will not typically call this directly. This method is registered with the low-level
         /// cluster data API and will be invoked automatically when running in a cluster.
         /// </remarks>
-        /// <param name="rawData"></param>
-        /// <returns></returns>
+        /// <param name="rawData">Raw data representing a serialized sequence of events of type <typeparamref name="TEvent"/></param>
+        /// <returns><see langword="true"/> if deserialized correctly and listeners did not throw.</returns>
         public bool DeserializeAndPublish(ReadOnlySpan<byte> rawData)
         {
-            var data = MemoryMarshal.Cast<byte, T>(rawData);
-            foreach (var item in data)
+            try
             {
-                foreach (var listener in m_Listeners)
+                var data = MemoryMarshal.Cast<byte, TEvent>(rawData);
+                foreach (var item in data)
                 {
-                    listener.Invoke(item);
+                    foreach (var listener in m_Listeners)
+                    {
+                        listener.Invoke(item);
+                    }
                 }
-            }
 
-            foreach (var bulkListener in m_BulkListeners)
+                foreach (var bulkListener in m_BulkListeners)
+                {
+                    bulkListener.Invoke(data);
+                }
+
+                return true;
+            }
+            catch (Exception e)
             {
-                bulkListener.Invoke(data);
+                ClusterDebug.LogError(e.Message);
+                return false;
             }
-
-            return true;
         }
 
         /// <summary>
         /// This is an overload provided to be compatible with the cluster data API.
         /// </summary>
-        /// <param name="rawData"></param>
-        /// <returns></returns>
+        /// <param name="rawData">Raw data representing a serialized sequence of events of type <typeparamref name="TEvent"/></param>
+        /// <returns><see langword="true"/> if deserialized correctly and listeners did not throw.</returns>
         public bool DeserializeAndPublish(NativeArray<byte> rawData) => DeserializeAndPublish(rawData.AsReadOnlySpan());
 
         /// <summary>
         /// Serialize enqueued events into a byte buffer and clears the queue.
         /// </summary>
-        /// <param name="outData"></param>
-        /// <returns></returns>
-        int SerializeAndFlush(NativeArray<byte> outData)
+        /// <param name="outBuffer">The buffer to hold the serialized events.</param>
+        /// <returns>The number of bytes copied to <paramref name="outBuffer"/>></returns>
+        int SerializeAndFlush(NativeArray<byte> outBuffer)
         {
             var bytes = MemoryMarshal.AsBytes(m_OutBuffer.GetSubArray(0, m_OutBufferLength).AsSpan());
-            var bytesWritten = bytes.TryCopyTo(outData.AsSpan()) ? bytes.Length : 0;
+            var bytesWritten = bytes.TryCopyTo(outBuffer.AsSpan()) ? bytes.Length : 0;
             m_OutBufferLength = 0;
             return bytesWritten;
         }
@@ -151,6 +160,27 @@ namespace Unity.ClusterDisplay
         {
             EmitterStateWriter.UnregisterCustomDataDelegate(k_FrameDataId);
             RepeaterStateReader.UnregisterOnLoadDataDelegate(k_FrameDataId);
+        }
+    }
+
+    static class EventHelperExtensions
+    {
+        /// <summary>
+        /// An implementation of the djb2 algorithm
+        /// http://www.cse.yorku.ca/~oz/hash.html
+        /// </summary>
+        public static int DeterministicTypeID(this Type type)
+        {
+            var name = type.AssemblyQualifiedName;
+            Debug.Assert(name != null, nameof(name) + " != null");
+
+            var hash = 5381UL;
+            foreach (var c in name)
+            {
+                hash = ((hash << 5) + hash) + c;
+            }
+
+            return (int)hash;
         }
     }
 }
