048712781de6722f2fd7499d7510ce7f40c695d7
Etienne Cella <etienne.p.cella@gmail.com>
2022-07-22 15:54:23 -0400

    [On Hold, Needs SRP Landing] Shader Stripping (#55)
    * add asset option
    
    * use preprocessor to check stripping
    
    * preprocessor not public
    
    * move to global settings

---
+++ source/com.unity.cluster-display.graphics/Editor/ShaderPreprocessor.cs
@@ -0,0 +1,56 @@
+﻿#if CLUSTER_DISPLAY_URP
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor.Build;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Unity.ClusterDisplay.Graphics.Editor
+{
+    class ShaderPreprocessor : IPreprocessShaders
+    {
+        public int callbackOrder => 0;
+
+        public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> data)
+        {
+            if (TryGetGlobalSettingsStripScreenCoordOverrideVariants(out var stripped) && stripped)
+            {
+                throw new InvalidOperationException(
+                    "Screen Coordinates Override shader variants are stripped from Player builds. " +
+                    "You can fix this by unselecting the \"Strip Screen Coord Override Variants\" checkbox " +
+                    "in the Universal Render Pipeline Global Settings.");
+            }
+        }
+
+        static bool TryGetGlobalSettingsStripScreenCoordOverrideVariants(out bool value)
+        {
+            try
+            {
+                var type = Type.GetType("UnityEngine.Rendering.Universal.UniversalRenderPipelineGlobalSettings, Unity.RenderPipelines.Universal.Runtime");
+                Assert.IsNotNull(type);
+
+                var instanceProp = type.GetProperty("instance", BindingFlags.Public | BindingFlags.Static);
+                Assert.IsNotNull(instanceProp);
+
+                var instance = instanceProp.GetValue(null);
+                Assert.IsNotNull(instance);
+
+                var settingProp = type.GetProperty("stripScreenCoordOverrideVariants", BindingFlags.Public | BindingFlags.Instance);
+                Assert.IsNotNull(settingProp);
+
+                value = (bool)settingProp.GetValue(instance);
+                return true;
+            }
+            catch
+            {
+                Debug.LogError("Could not read Universal Render Pipeline Global Settings.");
+            }
+
+            value = true;
+            return false;
+        }
+    }
+}
+#endif
