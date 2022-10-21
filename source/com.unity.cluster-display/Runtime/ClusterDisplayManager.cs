6dc9214494b922de1343e8101ddb664608e9b9a4
Wen Zhang <wen.zhang@unity3d.com>
2022-10-21 15:40:58 -0400

    Alternate ways of getting node ids

--- source/com.unity.cluster-display/Runtime/ClusterDisplayManager.cs
+++ source/com.unity.cluster-display/Runtime/ClusterDisplayManager.cs
@@ -1,4 +1,13 @@
-﻿using Unity.ClusterDisplay.Utils;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.Win32;
+using Unity.ClusterDisplay.Utils;
 using UnityEngine;
 
 namespace Unity.ClusterDisplay
@@ -7,9 +16,12 @@ namespace Unity.ClusterDisplay
     [DefaultExecutionOrder(1000)] // Make sure ClusterRenderer executes late.
     public class ClusterDisplayManager : SingletonMonoBehaviour<ClusterDisplayManager>
     {
+        [SerializeField]
+        bool m_IgnoreCommandLine;
+
         static ClusterSync GetOrCreateClusterSyncInstance()
         {
-            if (ClusterSyncInstance is not {} instance)
+            if (ClusterSyncInstance is not { } instance)
             {
                 // Creating ClusterSync instance on demand.
                 ClusterDebug.Log($"Creating instance of: {nameof(ClusterSync)} on demand.");
@@ -35,9 +47,75 @@ namespace Unity.ClusterDisplay
                 DontDestroyOnLoad(gameObject);
         }
 
-        private void OnEnable() => GetOrCreateClusterSyncInstance().EnableClusterDisplay(ClusterParams.FromCommandLine());
+        void OnEnable() => GetOrCreateClusterSyncInstance().EnableClusterDisplay(m_IgnoreCommandLine ? DetectSettings() : ClusterParams.FromCommandLine());
 
-        void CleanUp ()
+        static ClusterParams DetectSettings()
+        {
+            var clusterParams = new ClusterParams
+            {
+                Port = 25690,
+                MulticastAddress = "224.0.1.0",
+                ClusterLogicSpecified = true,
+                CommunicationTimeout = TimeSpan.FromSeconds(5),
+                HandshakeTimeout = TimeSpan.FromSeconds(10),
+                NodeID = 255
+            };
+
+            using var clusterKey = Registry.LocalMachine.OpenSubKey("Software\\Unity Technologies\\ClusterDisplay", true);
+            if (clusterKey != null)
+            {
+                clusterParams.NodeID = (byte)clusterKey.GetValue("NodeID");
+            }
+            else
+            {
+                var retries = 0;
+                var nodeIdFilePath = Path.Combine(Path.GetTempPath(), Application.productName + ".node");
+                while (clusterParams.NodeID == 255 && retries < 10)
+                {
+                    try
+                    {
+                        using var fileStream = new FileStream(nodeIdFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+                        using var reader = new StreamReader(fileStream);
+                        if (!byte.TryParse(reader.ReadLine(), out clusterParams.NodeID))
+                        {
+                            clusterParams.NodeID = 0;
+                        }
+
+                        fileStream.Position = 0;
+                        using var writer = new StreamWriter(fileStream);
+                        writer.Write(clusterParams.NodeID + 1);
+                        fileStream.SetLength(fileStream.Position);
+                    }
+                    catch (Exception ex)
+                    {
+                        retries++;
+                        ClusterDebug.Log($"Unable to access node ID file: {ex.Message}");
+                        Thread.Sleep(500);
+                    }
+                }
+
+                Application.quitting += () =>
+                {
+                    File.Delete(nodeIdFilePath);
+                };
+
+                ClusterDebug.Log($"Auto-assigning node ID {clusterParams.NodeID}");
+            }
+
+            if (clusterParams.NodeID == 255)
+            {
+                ClusterDebug.LogError("Cannot assign a node ID.");
+                clusterParams.ClusterLogicSpecified = false;
+            }
+
+            // First one to start up gets to be the emitter - node 0
+            clusterParams.EmitterSpecified = clusterParams.NodeID == 0;
+            clusterParams.RepeaterCount = clusterParams.EmitterSpecified ? 1 : 0;
+
+            return clusterParams;
+        }
+
+        void CleanUp()
         {
             ClusterSyncInstance?.DisableClusterDisplay();
             ServiceLocator.Withdraw<ClusterSync>();
