69efd34874d3d59adcf9298acd3d38e24a2c02f4
Wen Zhang <wen.zhang@unity3d.com>
2023-02-07 14:58:52 -0500

    Make ILooper (low-level util) internal

--- source/com.unity.live-editing/Runtime/Core/Utils/ILooper.cs
+++ source/com.unity.live-editing/Runtime/Core/Utils/ILooper.cs
@@ -5,7 +5,7 @@ namespace Unity.LiveEditing.LowLevel
     /// <summary>
     /// Provides a mechanism to perform recurring operations by subscribing to the <see cref="Update"/> event.
     /// </summary>
-    public interface ILooper
+    interface ILooper
     {
         public Action Update { get; set; }
     }
