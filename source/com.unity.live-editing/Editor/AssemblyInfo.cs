15224ed67e1d65ddef42488877e82be77ed13c5a
Scott Sewell <scott.sewell@unity3d.com>
2023-02-20 16:06:25 -0500

    initial testing setup

--- source/com.unity.live-editing/Editor/AssemblyInfo.cs
+++ source/com.unity.live-editing/Editor/AssemblyInfo.cs
@@ -3,9 +3,7 @@ using System.Runtime.CompilerServices;
 // Internal access needed for runtime assemblies
 
 // Internal access needed for editor assemblies
-[assembly: InternalsVisibleTo("Unity.LiveEditing.Editor")]
 
 // Internal access needed for testing
 [assembly: InternalsVisibleTo("InternalsVisible.ToDynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("Unity.LiveEditing.Tests.Editor")]
-[assembly: InternalsVisibleTo("Unity.LiveEditing.Tests.Runtime")]
---
+++ source/com.unity.live-editing/Tests/Editor/SceneChangeTrackerTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.LiveEditing.Editor;
+using UnityEditor.SceneManagement;
+
+namespace Unity.LiveEditing.Tests.Editor
+{
+    class SceneChangeTrackerTests
+    {
+        SceneSetup[] m_SceneSetups;
+        SceneChangeTracker m_SceneChangeTracker;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            m_SceneSetups = EditorSceneManager.GetSceneManagerSetup();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            EditorSceneManager.RestoreSceneManagerSetup(m_SceneSetups);
+        }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            m_SceneChangeTracker = new SceneChangeTracker();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (m_SceneChangeTracker != null)
+            {
+                m_SceneChangeTracker.Dispose();
+                m_SceneChangeTracker = null;
+            }
+        }
+        
+        [UnityTest]
+        public IEnumerator TestClientSendAndReceive()
+        {
+            yield break;
+        }
+    }
+}
