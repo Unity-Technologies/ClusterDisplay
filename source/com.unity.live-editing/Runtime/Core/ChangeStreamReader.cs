1da331c7c7bff37e74d52d59032924d30de55052
Scott Sewell <scott.sewell@unity3d.com>
2023-02-23 18:05:01 -0500

    remove unused files

--- source/com.unity.live-editing/Runtime/Core/ChangeStreamReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Unity.LiveEditing
-{
-    class ChangeStreamReader
-    {
-        readonly Stream m_Stream;
-    }
-}
--- source/com.unity.live-editing/Runtime/Core/ChangeStreamWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Unity.LiveEditing
-{
-    class ChangeStreamWriter
-    {
-        readonly Stream m_Stream;
-    }
-}
