18551ec4f669a0d6573332740adc427d901018fb
Wen Zhang <wen.zhang@unity3d.com>
2023-03-09 16:31:00 -0500

    Devices page

--- MissionControl-v2/MissionControl.EngineeringUI/Client/Pages/Devices.razor.cs
+++ MissionControl-v2/MissionControl.EngineeringUI/Client/Pages/Devices.razor.cs
@@ -13,8 +13,26 @@ namespace Unity.ClusterDisplay.MissionControl.EngineeringUI.Pages
         ComplexesService Complexes { get; set; } = default!;
         [Inject]
         DialogService DialogService { get; set; } = default!;
+        [Inject]
+        NotificationService NotificationService { get; set; } = default!;
 
         protected RadzenDataGrid<LaunchComplex> m_ComplexesGrid = default!;
+
+        IList<LaunchComplex>? m_SelectedComplexes;
+
+        bool HasSelection => Complexes.Collection.Values.Any(i => m_SelectedComplexes?.Contains(i) ?? false);
+
+        void SelectAllOrNone(bool all)
+        {
+            if (all)
+            {
+                m_SelectedComplexes = Complexes.Collection.Values.ToList();                
+            }
+            else
+            {
+                m_SelectedComplexes = null;
+            }
+        }
 
         protected override void OnInitialized()
         {
@@ -33,7 +51,6 @@ namespace Unity.ClusterDisplay.MissionControl.EngineeringUI.Pages
             await DialogService.OpenAsync<Dialogs.EditLaunchComplex>($"Edit {launchComplex.Name}",
                new Dictionary<string, object>() { { "ToEdit", launchComplex.DeepClone() } },
                new DialogOptions() { Width = "60%", Height = "80%", Resizable = true, Draggable = true });
-
         }
 
         protected async Task DeleteLaunchComplex(LaunchComplex launchComplex)
@@ -46,6 +63,25 @@ namespace Unity.ClusterDisplay.MissionControl.EngineeringUI.Pages
             }
 
             await Complexes.DeleteAsync(launchComplex.Id);
+            SelectAllOrNone(all: false);
+            NotificationService.Notify(severity: NotificationSeverity.Info, summary: $"Node \"{launchComplex.Name}\" was deleted.");
+        }
+
+        protected async Task DeleteSelectedLaunchComplex()
+        {
+            if (!HasSelection) return;
+            string names = String.Join("; ", m_SelectedComplexes!.Select(i => i.Name) ?? Enumerable.Empty<string>());
+
+            var ret = await DialogService.Confirm($"Do you want to delete the following complexes? {names}",
+    "Confirm deletion", new() { OkButtonText = "Yes", CancelButtonText = "No" });
+            if (!ret.HasValue || !ret.Value)
+            {
+                return;
+            }
+
+            await Task.WhenAll(m_SelectedComplexes!.Select(i => Complexes.DeleteAsync(i.Id)));
+            SelectAllOrNone(all: false);
+            NotificationService.Notify(severity: NotificationSeverity.Info, summary: $"Nodes deleted.");
         }
 
         public void Dispose()
