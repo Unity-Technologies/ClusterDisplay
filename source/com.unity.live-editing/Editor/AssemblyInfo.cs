0ae699f23114a07c3221a105ce66be56972d20a8
Wen Zhang <wen.zhang@unity3d.com>
2023-07-21 14:04:55 -0400

    Remove live-editing

--- source/com.unity.live-editing/Editor/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-// Internal access needed for runtime assemblies
-
-// Internal access needed for editor assemblies
-
-// Internal access needed for testing
-[assembly: InternalsVisibleTo("InternalsVisible.ToDynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("Unity.LiveEditing.Tests.Editor")]
--- source/com.unity.live-editing/README.md
+++
@@ -1,5 +0,0 @@
-# Live Editing
-
-The Live Editing package is used to remotely edit scenes from the editor.
-
-See the [package documentation](Documentation~/index.md) for more information.
